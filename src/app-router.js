9fd79ee8ef69ceaaae8631d2bcd606ffa8134790
Qiu Juntao <juntao.qiu@gmail.com>
2016-03-17 00:30:14 +1100

    add another route

--- src/app-router.js
+++ src/app-router.js
@@ -2,30 +2,47 @@ var Backbone = require('backbone');
 
 var ListItemCollection = require('./collections/list-item-collection');
 var ListView = require('./views/list-view');
+var ListItemView = require('./views/list-item-view');
+
+Backbone.View.prototype.close = function(){
+  this.remove();
+  this.unbind();
+  if (this.onClose){
+    this.onClose();
+  }
+}
 
 var AppRouter = Backbone.Router.extend({
   routes: {
-    "": "list"
+    "": "list",
+    "list": "list",
+    "list/:id": "listItem"
   },
 
   initialize: function(options){
     this.appView = options.appView;
+
+    this.collection = new ListItemCollection();
+    this.collection.reset([{
+      id:1, title: "Asia", description: "Hello darkness"
+    }, {
+      id:2, title: "Africa", description: "My old friend"
+    }]);
   },
 
   list: function(){
-    var collection = new ListItemCollection();
-
-    collection.reset([{
-      title: "Asia", description: "Hello darkness"
-    }, {
-      title: "Africa", description: "My old friend"
-    }]);
-
     var listView = new ListView({
-      collection: collection
+      collection: this.collection
     });
 
     this.appView.showView(listView);
+  },
+
+  listItem: function(id) {
+    var item = this.collection.get(id);
+    var listItemView = new ListItemView(item);
+
+    this.appView.showView(listItemView);
   }
 });
 
